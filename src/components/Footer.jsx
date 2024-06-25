6c01f8cf5c541b4a943afd5960ce78eafd090b8d
Vaibhav Agarwal <vjindal742@gmail.com>
2024-06-25 16:50:21 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="relative h-[100vh]"
+        <div className="relative h-[735px]"
             style={{ clipPath: `polygon(0% 0, 100% 0%, 100% 100%, 0 100%)` }}
         >
-            <div className="relative h-[200vh] -top-[100vh]">
-                <div className="h-[100vh] sticky top-0">
+            <div className="relative h-[calc(100vh+735px)] -top-[100vh]">
+                <div className="h-[735px] sticky top-[calc(100vh-735px)]">
                     <div className='Content rounded-3xl h-screen flex flex-col items-center justify-center font-["found"] uppercase text-7xl lg:text-[10vw] font-semibold bg-[#CEEA67] overflow-hidden'>
                         <h1>Ready</h1>
                         <h1>to start</h1>
